1d27bd7c855c3f8f48f21246f3d01f526b3a146f
PAZHANI-KV <pazhanivel11111@gmail.com>
2024-04-20 12:14:00 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Counter from './use_effect/Counter';
 import Component from './use_effect/Component';
+import Window from './use_effect/Window';
 
 function App() {
   return (
     <div className="App">
     <Counter />
     <Component />
+    <Window />
     </div>
   );
 }
